1e3b42f213972e0c0918956ff872d6a83c96dcb9
Amit kumar <kumaramit19100@gmail.com>
2022-04-29 14:02:10 +0530

    Initial Commit

---
+++ TimerFunction_29April/Function1.cs
@@ -0,0 +1,41 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using CloudStorageAccount = Microsoft.WindowsAzure.Storage.CloudStorageAccount;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+
+namespace TimerFunction_29April
+{
+    public class LogEntity : TableEntity
+    {
+        public string Number { get; set; }
+    }
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            CloudStorageAccount cls = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("ConnectionString", EnvironmentVariableTarget.Process));
+            CloudQueueClient queueClient = cls.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference(Environment.GetEnvironmentVariable("QueueName",EnvironmentVariableTarget.Process));
+            CloudQueueMessage queueMessage =await queue.GetMessageAsync();
+
+            if (queueMessage != null)
+            {
+                CloudTableClient tableClient = cls.CreateCloudTableClient();
+                CloudTable table = tableClient.GetTableReference(Environment.GetEnvironmentVariable("TableName",EnvironmentVariableTarget.Process));
+                TableQuery<LogEntity> tableQuery = new TableQuery<LogEntity>();
+                foreach(LogEntity entity in await table.ExecuteQuerySegmentedAsync(tableQuery, null))
+                {
+                    Console.WriteLine($"{entity.PartitionKey} \t{entity.RowKey}\t {entity.Number}");
+                }
+                Console.WriteLine(queueMessage.AsString);
+                //await cloudQueue.DeleteMessageAsync(message.Id, message.PopReceipt);
+            }
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
